c865d413fe03f5523308a7d3fa30908cc5a3be4e
João Salgado <jgs1884@gmail.com>
2018-09-04 23:37:31 -0300

    correct api environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-diagnosis.herokuapp.com/',
+  apiUrl: 'https://api-diagnosis.herokuapp.com',
 
-  tokenWhitelistedDomains: [ new RegExp(/api-stage-diagnosis/) ],
+  tokenWhitelistedDomains: [ new RegExp(/api-diagnosis/) ],
   tokenBlacklistedRoutes: [new RegExp(/\/oauth\/token/), new RegExp(/\/company-processes/)]
 };
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-stage-diagnosis.herokuapp.com/',
+  apiUrl: 'https://api-stage-diagnosis.herokuapp.com',
 
   tokenWhitelistedDomains: [ new RegExp(/api-stage-diagnosis/) ],
   tokenBlacklistedRoutes: [new RegExp(/\/oauth\/token/), new RegExp(/\/company-processes/)]
