8ed56767261d3145cefba8f641c6d68303b87468
João Salgado <jgs1884@gmail.com>
2018-09-07 16:56:49 -0300

    complement authorization

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api-diagnosis.herokuapp.com',
-  authorization: 'Basic ckc2R0NIRTkzald6d3ZLVDp0bTQ0TDJwcENIM1A2S1Z2ejhuYQ',
+  authorization: 'Basic ckc2R0NIRTkzald6d3ZLVDp0bTQ0TDJwcENIM1A2S1Z2ejhuYQ==',
 
   tokenWhitelistedDomains: [ new RegExp(/api-diagnosis/) ],
   tokenBlacklistedRoutes: [new RegExp(/\/oauth\/token/), new RegExp(/\/company-processes/)]
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api-stage-diagnosis.herokuapp.com',
-  authorization: 'Basic ckc2R0NIRTkzald6d3ZLVDp0bTQ0TDJwcENIM1A2S1Z2ejhuYQ',
+  authorization: 'Basic ckc2R0NIRTkzald6d3ZLVDp0bTQ0TDJwcENIM1A2S1Z2ejhuYQ==',
 
   tokenWhitelistedDomains: [ new RegExp(/api-stage-diagnosis/) ],
   tokenBlacklistedRoutes: [new RegExp(/\/oauth\/token/), new RegExp(/\/company-processes/)]
