7acd0b24092108a2e278b0b6014335b6c47699e0
João Salgado <jgs1884@gmail.com>
2018-09-23 22:02:42 -0300

    removed unused files

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -86,21 +86,6 @@ export const routes: Routes = [
         canActivate: [AuthGuard]
       },
       {
-        path: 'base',
-        loadChildren: './views/base/base.module#BaseModule',
-        canActivate: [AuthGuard]
-      },
-      {
-        path: 'buttons',
-        loadChildren: './views/buttons/buttons.module#ButtonsModule',
-        canActivate: [AuthGuard]
-      },
-      {
-        path: 'charts',
-        loadChildren: './views/chartjs/chartjs.module#ChartJSModule',
-        canActivate: [AuthGuard]
-      },
-      {
         path: 'dashboard',
         loadChildren: './views/dashboard/dashboard.module#DashboardModule',
         canActivate: [AuthGuard]
@@ -108,21 +93,6 @@ export const routes: Routes = [
       {
         path: 'convites',
         loadChildren: './views/invite/invite.module#InviteModule',
-        canActivate: [AuthGuard]
-      },
-      {
-        path: 'icons',
-        loadChildren: './views/icons/icons.module#IconsModule',
-        canActivate: [AuthGuard]
-      },
-      {
-        path: 'notifications',
-        loadChildren: './views/notifications/notifications.module#NotificationsModule',
-        canActivate: [AuthGuard]
-      },
-      {
-        path: 'widgets',
-        loadChildren: './views/widgets/widgets.module#WidgetsModule',
         canActivate: [AuthGuard]
       },
       { path: '**', redirectTo: '/404' },
